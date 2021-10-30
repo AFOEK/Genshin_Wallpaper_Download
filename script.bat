4db841e8fefc3db3ec218b4d5ae2ea7f4a760772
AFOEK <felix.montalfu@gmail.com>
2021-10-30 12:23:29 +0700

    Adding Checksum and compression

--- script.bat
+++ script.bat
@@ -6,3 +6,6 @@ for /L %%i in (1,1,39) do (
     curl --output %%i.jpg %web_root%%%i.jpg
 )
 "C:\Program Files\7-Zip\7z.exe" a img_compressed.7z *.jpg
+"C:\Program Files\WinRAR\Rar.exe" -M5 a img_compressed.rar *.jpg
+certUtil -hashfile *.jpg SHA256
+certUtil -hashfile img_compressed.* SHA256
