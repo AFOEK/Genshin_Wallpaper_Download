e89c2241a79de46a5772f71c7077745c3624dcdb
AFOEK <felix.montalfu@gmail.com>
2021-10-30 09:40:40 +0700

    Fixing for loop batch script

--- script.bat
+++ script.bat
@@ -2,4 +2,7 @@
 set web_root=https://genshin-wallpaper.web.app/img/wallpapers/
 mkdir imgs
 cd imgs
-for /L %%i in (1,1,39) do wget %web_root%%i%.jpg
+for /L %%i in (1,1,39) do (
+    curl --output %%i.jpg %web_root%%%i.jpg
+)
+"C:\Program Files\7-Zip\7z.exe" a img_compressed.7z *.jpg
--- script.sh
+++ script.sh
@@ -5,13 +5,13 @@ cd imgs
 for ((i = 1 ; i <= 39 ; i++)); do
     wget "${web_root}$i.jpg"
 done
-tar czvf img_compressed.tar.gz *.jpg
-tar cjvf img_compressed.tar.bz *.jpg
-zip img_compressed.zip *.jpg
-rar a img_compressed.rar *.jpg
-tar cvf img_compressed.tar.xz --use-compress-program='xz -9 -vT12' *.jpg
-7z a img_compressed.7z *jpg
+# tar czvf img_compressed.tar.gz *.jpg
+# tar cjvf img_compressed.tar.bz *.jpg
+# zip img_compressed.zip *.jpg
+# rar a img_compressed.rar *.jpg
+# tar cvf img_compressed.tar.xz --use-compress-program='xz -9 -vT12' *.jpg
+# 7z a img_compressed.7z *.jpg
 sha256sum *.jpg > checksum_jpg_individual
-sha256sum img_compressed.* > checksum_jpg_compressed
+# sha256sum img_compressed.* > checksum_jpg_compressed
 sha256sum --check checksum_jpg_individual
-sha256sum --check checksum_jpg_compressed
+# sha256sum --check checksum_jpg_compressed
